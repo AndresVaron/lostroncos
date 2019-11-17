07bbe2e5526604d89cb623f4e534962dc7897e4c
andresvaron <af.varon@uniandes.edu.co>
2019-11-16 20:34:47 -0500

    test

--- src/MapaPrincipal/MapaPrincipal.jsx
+++ src/MapaPrincipal/MapaPrincipal.jsx
@@ -8,7 +8,11 @@ class MapaPrincipal extends React.Component {
     super(props);
     this.state = {
       id: this.props.match.params.id,
-      ingreso: false
+      ingreso: false,
+      info: null,
+      cargado: false,
+      google: null,
+      map: null
     };
   }
 
@@ -26,92 +30,143 @@ class MapaPrincipal extends React.Component {
         ingreso: false
       });
     }
-    let info = {
-      poly: [
-        { lat: 2.552654, lng: -72.836102 },
-        { lat: 2.544609, lng: -72.832424 },
-        { lat: 2.547288, lng: -72.822045 },
-        { lat: 2.558031, lng: -72.825027 },
-        { lat: 2.558028, lng: -72.825028 }
-      ]
-    };
+    if (!this.state.ingreso) {
+      let info = {
+        poly: [
+          { lat: 2.552654, lng: -72.836102 },
+          { lat: 2.544609, lng: -72.832424 },
+          { lat: 2.547288, lng: -72.822045 },
+          { lat: 2.558031, lng: -72.825027 },
+          { lat: 2.558028, lng: -72.825028 }
+        ],
+        steps: [
+          { lat: 2.579775, lng: -72.760284 },
+          { lat: 3.502403, lng: -73.690598 },
+          { lat: 4.08584, lng: -73.674188 },
+          { lat: 7.05332, lng: -73.082078 },
+          { lat: 10.357106, lng: -75.4434 },
+          { lat: 36.881967, lng: -76.271164 },
+          { lat: 42.295881, lng: -75.39428 },
+          { lat: 40.7160658, lng: -73.9863171 }
+        ]
+      };
+      //Aqui se hace el fetch
+      this.setState({ info: info });
+    } else {
+    }
   }
 
   handleGoogleMapApi(google) {
     var LatLng = google.maps.LatLng,
       LatLngBounds = google.maps.LatLngBounds,
       Marker = google.maps.Marker,
+      Polygon = google.maps.Polygon,
       Point = google.maps.Point;
     const map = google.map;
-
-    var pos1 = new LatLng(23.634501, -102.552783);
-    var pos2 = new LatLng(17.987557, -92.929147);
-    var pos3 = new LatLng(12.987557, -92.929147);
-    var pos4 = new LatLng(17.987557, -98.929147);
-
-    var bounds = new LatLngBounds();
-    bounds.extend(pos1);
-    bounds.extend(pos2);
-    bounds.extend(pos3);
-    bounds.extend(pos4);
-    map.fitBounds(bounds);
-
-    var markerP1 = new Marker({
-      position: pos1,
-      map: map
-    });
-    var markerP2 = new Marker({
-      position: pos2,
-      map: map
-    });
-    this.pintarSalto(markerP1, markerP2, map, google);
-
-    setTimeout(
-      () => {
-        var markerP3 = new Marker({
-          position: pos3,
-          map: map
+    this.setState({ google: google, map: map });
+    if (!this.state.cargado) {
+      this.setState({ cargado: true });
+      var pos = this.state.info.poly;
+      if (pos !== undefined) {
+        var polygono = new Polygon({
+          paths: pos,
+          strokeColor: "#FF0000",
+          strokeOpacity: 0.8,
+          strokeWeight: 3,
+          fillColor: "#FF0000",
+          fillOpacity: 0.35
         });
-        this.pintarSalto(markerP2, markerP3, map, google);
-        setTimeout(() => {
-          var markerP4 = new Marker({
-            position: pos4,
-            map: map
-          });
-          this.pintarSalto(markerP3, markerP4, map, google);
-        }, 2000);
-      },
+        polygono.setMap(this.state.map);
+        var bounds = new LatLngBounds();
+        this.state.info.poly.forEach(element => {
+          bounds.extend(new LatLng(element.lat, element.lng));
+        });
 
-      2000
-    );
+        setTimeout(() => {
+          this.state.map.panTo(bounds.getCenter());
+          setTimeout(() => {
+            this.state.map.setZoom(13);
+          }, 1000);
+        }, 3000);
+      }
+    }
+
+    // var pos1 = new LatLng(23.634501, -102.552783);
+    // var pos2 = new LatLng(17.987557, -92.929147);
+    // var pos3 = new LatLng(12.987557, -92.929147);
+    // var pos4 = new LatLng(17.987557, -98.929147);
+
+    // var bounds = new LatLngBounds();
+    // bounds.extend(pos1);
+    // bounds.extend(pos2);
+    // bounds.extend(pos3);
+    // bounds.extend(pos4);
+    // map.fitBounds(bounds);
+
+    // var markerP1 = new Marker({
+    //   position: pos1,
+    //   map: map
+    // });
+    // var markerP2 = new Marker({
+    //   position: pos2,
+    //   map: map
+    // });
+    // this.pintarSalto(markerP1, markerP2, map, google);
+
+    // setTimeout(
+    //   () => {
+    //     var markerP3 = new Marker({
+    //       position: pos3,
+    //       map: map
+    //     });
+    //     this.pintarSalto(markerP2, markerP3, map, google);
+    //     setTimeout(() => {
+    //       var markerP4 = new Marker({
+    //         position: pos4,
+    //         map: map
+    //       });
+    //       this.pintarSalto(markerP3, markerP4, map, google);
+    //     }, 2000);
+    //   },
+
+    //   2000
+    // );
   }
 
   render() {
-    return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-12 noPadding">
-            <div className="contenedorPrincipal">
-              <div style={{ height: "100vh", width: "100%" }}>
-                <GoogleMapReact
-                  bootstrapURLKeys={{
-                    key: "AIzaSyAEN10VnQRdN67sDTlx-mfWapGXB8rLMas",
-                    libraries: ["drawing,geometry"].join(",")
-                  }}
-                  defaultCenter={{
-                    lat: this.props.myLocation.lat,
-                    lng: this.props.myLocation.lng
-                  }}
-                  defaultZoom={this.props.myLocation.zoom}
-                  yesIWantToUseGoogleMapApiInternals
-                  onGoogleApiLoaded={this.handleGoogleMapApi.bind(this)}
-                ></GoogleMapReact>
+    if (this.state.info === null) {
+      return (
+        <div className="container-fluid">
+          <div className="row"></div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12 noPadding">
+              <div className="contenedorPrincipal">
+                <div style={{ height: "100vh", width: "100%" }}>
+                  <GoogleMapReact
+                    bootstrapURLKeys={{
+                      key: "AIzaSyAEN10VnQRdN67sDTlx-mfWapGXB8rLMas",
+                      libraries: ["drawing,geometry"].join(",")
+                    }}
+                    defaultCenter={{
+                      lat: -5.9040035,
+                      lng: -65.9402055
+                    }}
+                    defaultZoom={4.5}
+                    yesIWantToUseGoogleMapApiInternals
+                    onGoogleApiLoaded={this.handleGoogleMapApi.bind(this)}
+                  ></GoogleMapReact>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 
   pintarSalto(markerP1, markerP2, map, google) {
